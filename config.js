5791482770de88bcc5c8beb37097aca3fb5b25fc
sijubahputih <kimie.ummah@gmail.com>
2026-07-12 00:28:41 +0800

    config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const wedding = {
+
+groomFirst:"ALIF HAIQAL",
+
+groomLast:"BIN NAZRI",
+
+brideFirst:"ARIESYAA ARYANA",
+
+brideLast:"BINTI MOHD ZAILANI",
+
+date:"19 Disember 2026",
+
+day:"Sabtu"
+
+};
