c8decd13bceaff123e7219d6f0d998085c38686e
sijubahputih <kimie.ummah@gmail.com>
2026-07-12 00:30:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,51 @@
 const wedding = {
 
-groomFirst:"ALIF HAIQAL",
+    // ==========================
+    // PENGANTIN
+    // ==========================
 
-groomLast:"BIN NAZRI",
+    groomFirst: "ALIF HAIQAL",
+    groomLast: "BIN NAZRI",
 
-brideFirst:"ARIESYAA ARYANA",
+    brideFirst: "ARIESYAA ARYANA",
+    brideLast: "BINTI MOHD ZAILANI",
 
-brideLast:"BINTI MOHD ZAILANI",
+    // ==========================
+    // MAJLIS
+    // ==========================
 
-date:"19 Disember 2026",
+    title: "Walimatul Urus",
 
-day:"Sabtu"
+    day: "Sabtu",
+
+    date: "19 Disember 2026",
+
+    time: "11:00 Pagi",
+
+    countdown: "2026-12-19T11:00:00",
+
+    // ==========================
+    // TEMPAT
+    // ==========================
+
+    venue: "Masukkan lokasi majlis",
+
+    maps: "https://maps.app.goo.gl/",
+
+    // ==========================
+    // RSVP
+    // ==========================
+
+    whatsapp: "601135705157",
+
+    // ==========================
+    // HADIAH
+    // ==========================
+
+    bankName: "Maybank",
+
+    accountName: "Nama Pengantin",
+
+    accountNumber: "1234567890"
 
 };
